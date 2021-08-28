21895ca4e9831dfe387cf57a250a068ef018cc40
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-08-27 17:26:17 -0700

    Add files via upload

--- docs/binary_converter_gui.js
+++ docs/binary_converter_gui.js
@@ -2,6 +2,7 @@
 
 var string = '';
 var lines = 0;
+var sendstring = '';
 
 function setup() {
   createCanvas(2000,875);
@@ -13,7 +14,10 @@ function draw() {
   fill(0);
   rect(0,0,700,850);
   textSize(150);
-  text(string,1100,750 - lines);
+  if (sendstring.length >= 12){
+    textSize(150-(5*(sendstring.length-10)));
+  }
+  text(string,850,750 - lines);
   if (mouseX > 20 && mouseX < 220 && mouseY > 20 && mouseY < 220){
     fill(0,0,200);
   } else {
@@ -74,30 +78,30 @@ function draw() {
     fill(125);
   }
   rect(20,680,200,150);
-  if (mouseX > 240 && mouseX < 440 && mouseY > 680 && mouseY < 750){
+  if (mouseX > 240 && mouseX < 440 && mouseY > 680 && mouseY < 830){
     fill(0,0,200);
   } else {
     fill(200,0,0);
   }
-  rect(240,680,200,70);
-  if (mouseX > 240 && mouseX < 440 && mouseY > 760 && mouseY < 830){
+  rect(240,680,200,150);
+  if (mouseX > 460 && mouseX < 660 && mouseY > 680 && mouseY < 830){
     fill(0,0,200);
   } else {
     fill(200,0,0);
   }
-  rect(240,760,200,70);
-  if (mouseX > 460 && mouseX < 660 && mouseY > 680 && mouseY < 750){
-    fill(0,0,200);
+  rect(460,680,200,150);
+  if (mouseX > 700 && mouseX < 825 && mouseY > 0 && mouseY < 425){
+    fill(255,255,0);
   } else {
     fill(0,200,0);
   }
-  rect(460,680,200,70);
-  if (mouseX > 460 && mouseX < 660 && mouseY > 760 && mouseY < 830){
-    fill(0,0,200);
+  rect(700,0,125,425);
+  if (mouseX > 700 && mouseX < 825 && mouseY > 425 && mouseY < 850){
+    fill(255,255,0);
   } else {
     fill(0,200,0);
   }
-  rect(460,760,200,70);
+  rect(700,425,125,425);
   fill(255);
   textSize(150);
   text('7',75,175);
@@ -111,69 +115,85 @@ function draw() {
   text('3',475,595);
   text('0',75,800);
   textSize(45);
-  text('Delete',275,740);
-  text('Reset',275,810);
-  text('Binary',475,740);
-  text('Decimal',475,810);
+  text('Delete',275,780);
+  text('Reset',475,780);
+  textSize(30);
+  fill(0);
+  text('Binary',710,200);
+  text('Decimal',710,600);
+  fill(255);
 }
 
 function mousePressed(){
   if (mouseX > 20 && mouseX < 220 && mouseY > 20 && mouseY < 220){
+    sendstring = sendstring + '7';
     string = string + '7';
   }
   rect(20,20,200,200);
   if (mouseX > 240 && mouseX < 440 && mouseY > 20 && mouseY < 220){
+    sendstring = sendstring + '8';
     string = string + '8';
   }
   rect(240,20,200,200);
   if (mouseX > 460 && mouseX < 660 && mouseY > 20 && mouseY < 220){
+    sendstring = sendstring + '9';
     string = string + '9';
   }
   rect(460,20,200,200);
   if (mouseX > 20 && mouseX < 220 && mouseY > 240 && mouseY < 440){
+    sendstring = sendstring + '4';
     string = string + '4';
   }
   rect(20,240,200,200);
   if (mouseX > 240 && mouseX < 440 && mouseY > 240 && mouseY < 440){
+    sendstring = sendstring + '5';
     string = string + '5';
   }
   rect(240,240,200,200);
   if (mouseX > 460 && mouseX < 660 && mouseY > 240 && mouseY < 440){
+    sendstring = sendstring + '6';
     string = string + '6';
   }
   rect(460,240,200,200);
   if (mouseX > 20 && mouseX < 220 && mouseY > 460 && mouseY < 660){
+    sendstring = sendstring + '1';
     string = string + '1';
   }
   rect(20,460,200,200);
   if (mouseX > 240 && mouseX < 440 && mouseY > 460 && mouseY < 660){
+    sendstring = sendstring + '2';
     string = string + '2';
   }
   rect(240,460,200,200);
   if (mouseX > 460 && mouseX < 660 && mouseY > 460 && mouseY < 660){
+    sendstring = sendstring + '3';
     string = string + '3';
   }
   rect(460,460,200,200);
   if (mouseX > 20 && mouseX < 220 && mouseY > 680 && mouseY < 830){
+    sendstring = sendstring + '0';
     string = string + '0';
   }
   rect(20,680,200,150);
-  if (mouseX > 240 && mouseX < 440 && mouseY > 680 && mouseY < 750){
+  if (mouseX > 240 && mouseX < 440 && mouseY > 680 && mouseY < 830){
     string = string.slice(0, -1);
+    sendstring = sendstring.slice(0, -1);
   }
   rect(240,680,200,70);
-  if (mouseX > 240 && mouseX < 440 && mouseY > 760 && mouseY < 830){
+  if (mouseX > 460 && mouseX < 660 && mouseY > 680 && mouseY < 830){
     string = '';
     lines = 0;
   }
   rect(240,760,200,70);
-  if (mouseX > 460 && mouseX < 660 && mouseY > 680 && mouseY < 750){
-    let num = string;
+  if (mouseX > 700 && mouseX < 825 && mouseY > 0 && mouseY < 425){
+    let num = sendstring;
+    string = string + '  -> B';
     text(getbinary(num),1000,500);
   }
   rect(460,680,200,70);
-  if (mouseX > 460 && mouseX < 660 && mouseY > 760 && mouseY < 830){
-    let num = string;
+  if (mouseX > 700 && mouseX < 825 && mouseY > 425 && mouseY < 850){
+    let num = sendstring;
+    string = string + '  -> D';
     text(getdecimal(num),1000,500);
   }
 }
@@ -191,7 +211,10 @@ function getbinary(num){
   }
   bit = parseInt(bit/2);
   
-  while (bit >= 1){
+  var isbinary = true;
+  
+  if (isbinary){
+  while (bit >= 1 ){
     binsum = binsum - bit;
     binary = binary + '1';
     
@@ -207,9 +230,11 @@ function getbinary(num){
     }
   }
 }
+string = string + '\n' + binary + '\n';
+  }
   print(binary);
-  string = string + '\n' + binary + '\n';
-  lines = lines + 150;
+  lines = lines + 375;
+  sendstring = '';
   return binary;
 }
 
@@ -221,7 +246,17 @@ function getdecimal(num){
 
   num = num;
   let x = 0;
- 
+  
+  var counter = 0;
+  var isbinary = true;
+  var scan = '';
+  while (counter < 100){
+    scan = num.slice(counter,counter+1);
+    if (scan != '1' && scan != '0'){
+      isbinary = false;
+    }
+    counter = counter + 1;
+  }
   while (x <= 100){
       try{
         element = num.slice(index-1,index);
@@ -239,8 +274,14 @@ function getdecimal(num){
     }
   if (num % 2 == 0){
     decimal = decimal - 1;
+  } 
+  
+  if (isbinary == false){
+    string = string + '\nErr: input not binary\n';
+  } else {
+   string = string + '\n' + decimal + '\n';
   }
-  string = string + '\n' + decimal + '\n';
-  lines = lines + 150;
+  lines = lines + 375;
+  sendstring = '';
   return decimal;
 }
